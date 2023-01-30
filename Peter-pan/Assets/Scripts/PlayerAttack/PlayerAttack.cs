a0d62237b04aad388655efd8858b4a866c5248c9
gleufeurg <102815589+gleufeurg@users.noreply.github.com>
2023-01-30 14:53:37 +0100

    Anims good
    Bonnes anims pour le player (et aussi pour les pirates mais c'est pas fait encore)
    
    + Debug du transform du player qui était bizarre à cause des anims
    
    (vérifier si attackArea est bien placé maintenant je sais plus mais je crois que son y est bien en 0 (ce qui est bon))

--- Peter-pan/Assets/Scripts/PlayerAttack/PlayerAttack.cs
+++ Peter-pan/Assets/Scripts/PlayerAttack/PlayerAttack.cs
@@ -4,46 +4,16 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    [SerializeField] private GameObject attackArea = default;
-    [SerializeField] private Animator animator;
-
-    [SerializeField] private bool attacking = false;
-
-    [SerializeField] [Range(0,10)] private float timeToAttack = 0.25f;
-    [SerializeField] private float timer = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        attackArea = transform.GetChild(0).gameObject;
-        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E))
-        {
-            Attack();
-        }
-
-        if(attacking)
-        {
-            timer += Time.deltaTime;
-
-            if(timer >= timeToAttack)
-            {
-                timer = 0;
-                attacking = false;
-                attackArea.SetActive(attacking);
-            }
-        }
+        
     }
 
-    private void Attack()
-    {
-        attacking = true;
-        attackArea.SetActive(attacking);
-        animator.Play("Stab");
-    }
 }
--- Peter-pan/Assets/Scripts/PlayerMovement.cs
+++ Peter-pan/Assets/Scripts/PlayerMovement.cs
@@ -4,24 +4,56 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Range(0, 1000)] public float movementSpeed;
-    [Range(0, 50)] public float jumpforce;
+    [Space(20f)]
+    [Header("References")]
+
+    //Refs
+    [SerializeField] private GameObject attackArea = default;
+    [SerializeField] private Animator animator;
 
     public Rigidbody rb;
 
-    private Vector3 velocity = Vector3.zero;
+    [Space(25f)]
+    [Header("Animations")]
 
-    // Start is called before the first frame update
+    //Animation States
+    const string PLAYER_IDLE = "Idle";
+    const string PLAYER_STAB = "Stab";
+    const string PLAYER_SLASH = "Inward Slash";
+    const string PLAYER_HEAVYATTACK = "Heavy Attack";
+    const string PLAYER_BLOCK = "Block";
+    const string PLAYER_RUN = "Slow Run";
+    const string PLAYER_DEATH = "Death";
+    const string PLAYER_TAUNT = "Taunt Military";
+
+    [Space(20f)]
+    [Header("Stats")]
+
+    //Stats
+    [SerializeField] [Range(0, 10)] private float timeToAttack = 0.25f;
+    [Range(0, 1000)] public float movementSpeed;
+    [Range(0, 50)] public float jumpforce;
+
+    [Space(20f)]
+    [Header("Others to verify")]
+
+    //Others
+    [SerializeField] private bool attacking = false;
+    [SerializeField] private bool isGrounded = true;
+    [SerializeField] private int groundMask;
+    [SerializeField] private string currentState;
+    [SerializeField] [Range(0, 10)] private float timer = 0f;
+    [SerializeField] private Vector3 velocity = Vector3.zero;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-    }
+        animator = GetComponent<Animator>();
+        attackArea = transform.GetChild(0).gameObject;
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        //Movements
-        Movement();
+        //Be sure to be idle from the start
+        ChangeAnimationState(PLAYER_IDLE);
     }
 
     private void Update()
@@ -32,20 +64,149 @@ public class PlayerMovement : MonoBehaviour
             Jump();
             //Debug.Log("bipbip");
         }
+
+        //Input actions
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Block();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Stab();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Slash();
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            HeavyAttack();
+        }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            Taunt();
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Death();
+        }
+
+        if (attacking)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= timeToAttack)
+            {
+                timer = 0;
+                attacking = false;
+                attackArea.SetActive(attacking);
+            }
+        }
     }
 
-    private void Movement()
+    void FixedUpdate()
     {
+        //Get Horizontal & Vertical Axis
         float hMovement = Input.GetAxisRaw("Horizontal") * movementSpeed * Time.deltaTime;
         float vMovement = Input.GetAxisRaw("Vertical") * movementSpeed * Time.deltaTime;
-        velocity = new Vector3(hMovement, rb.velocity.y, vMovement);
+
+        Debug.Log(hMovement);
+        Debug.Log(vMovement);
+
+        //Change the animation state from Run to Idle & Idle to Run
+        if (hMovement >= 0.25f || vMovement >= 0.25f)
+        {
+            ChangeAnimationState(PLAYER_RUN);
+        }
+        else if (hMovement <= -0.25f || vMovement <= -0.25f)
+        {
+            ChangeAnimationState(PLAYER_RUN);
+        }
+        else if(hMovement >= -0.25f && vMovement >= -0.25f && hMovement <= 0.25f && vMovement <= 0.25f)
+        {
+            ChangeAnimationState(PLAYER_IDLE);
+        }
+
+        //Movements
+        Movement(hMovement, vMovement);
+
+        //Ground check
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.3f, groundMask);
+        if (hit.collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void Movement(float _hMovement, float _vMovement)
+    {
+        velocity = new Vector3(_hMovement, rb.velocity.y, _vMovement);
 
         rb.velocity = velocity;
+        
     }
 
     private void Jump()
     {
         velocity = new Vector3(velocity.x, jumpforce, velocity.z);
         rb.velocity = velocity;
+    }
+
+    private void Stab()
+    {
+        attacking = true;
+        attackArea.SetActive(attacking);
+        ChangeAnimationState(PLAYER_STAB);
+    }
+    
+    private void Taunt()
+    {
+        attacking = true;
+        attackArea.SetActive(attacking);
+        ChangeAnimationState(PLAYER_TAUNT);
+    }
+
+    private void Slash()
+    {
+        attacking = true;
+        attackArea.SetActive(attacking);
+        ChangeAnimationState(PLAYER_SLASH);
+    }
+
+    private void HeavyAttack()
+    {
+        attacking = true;
+        attackArea.SetActive(attacking);
+        ChangeAnimationState(PLAYER_HEAVYATTACK);
+    }
+
+    private void Block()
+    {
+        attacking = true;
+        attackArea.SetActive(attacking);
+        ChangeAnimationState(PLAYER_BLOCK);
+    }
+
+    private void Death()
+    {
+        attacking = true;
+        attackArea.SetActive(attacking);
+        ChangeAnimationState(PLAYER_DEATH);
+    }
+
+    private void ChangeAnimationState(string newState)
+    {
+        //Prevent an animation to override itself
+        if (currentState == newState) return;
+
+        //Play the new animation
+        animator.Play(newState);
+
+        //Change the current state animation to the new animation
+        currentState = newState;
     }
 }
